7d2c3fc9236e0f986db1d591c2e3b150ce14c469
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-06-01 22:04:44 -0300

    feat: criando pagina para listar os serviços

--- frontend/barber/src/app/app.routes.ts
+++ frontend/barber/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AgendamentoComponent } from './pages/agendamento/agendamento.component';
 import { IndexComponent } from './pages/index/index.component';
+import { ServicosComponent } from './pages/servicos/servicos.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: "agendamento",
         component: AgendamentoComponent
+    },
+    {
+        path: "servicos",
+        component: ServicosComponent
     }
 ];
