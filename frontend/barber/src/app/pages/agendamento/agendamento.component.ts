2e00ae2812ba9b557964729fbf02e174a003a54a
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-06-04 22:48:15 -0300

    feat: adicionando formulario para agendamento

--- frontend/barber/src/app/pages/agendamento/agendamento.component.ts
+++ frontend/barber/src/app/pages/agendamento/agendamento.component.ts
@@ -1,20 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VoltarBtnComponent } from "../../components/voltar-btn/voltar-btn.component";
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agendamento',
-  imports: [VoltarBtnComponent],
+  imports: [VoltarBtnComponent, ReactiveFormsModule],
   templateUrl: './agendamento.component.html',
   styleUrl: './agendamento.component.scss'
 })
-export class AgendamentoComponent {
+export class AgendamentoComponent implements OnInit {
   servico: string;
+  today: string;
+  agendamentoForm!: FormGroup;
 
   constructor(private router: Router){
     const navigation = router.getCurrentNavigation();
     this.servico = navigation?.extras.state?.['servico'];
     console.log(this.servico);
+
+    const data = new Date();
+    this.today = data.toJSON().slice(0,10);
+    console.log(this.today)
+  
+    this.fetchAgendamentosPorDia()
+  }
+  
+  ngOnInit(): void {
+    this.agendamentoForm = new FormGroup({
+      data: new FormControl<Date | null>(null, [Validators.required, Validators.nullValidator]),
+      horario: new FormControl<string>("", [Validators.required, Validators.minLength(5)]),
+      cliente: new FormControl<string>("", [Validators.required, Validators.minLength(3), Validators.maxLength(50)])
+    })  
+  }
+
+  fetchAgendamentosPorDia(){
+    console.log(this.agendamentoForm?.get("data")?.value)
+    //falta passar o valor do input date para fazer a requisição
+    
+  }
+
+  onSubmit(){
+    console.log("implementar")
+    console.log(this.agendamentoForm.value)
   }
 
 }
