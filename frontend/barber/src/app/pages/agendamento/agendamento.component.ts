7f279d7d0aba053a54db3860996761968871324c
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-06-05 22:13:04 -0300

    feat: adicionando mais validaçoes nos forms

--- frontend/barber/src/app/pages/agendamento/agendamento.component.ts
+++ frontend/barber/src/app/pages/agendamento/agendamento.component.ts
@@ -21,23 +21,39 @@ export class AgendamentoComponent implements OnInit {
 
     const data = new Date();
     this.today = data.toJSON().slice(0,10);
-    console.log(this.today)
-  
-    this.fetchAgendamentosPorDia()
+    console.log("today"+this.today)
   }
   
   ngOnInit(): void {
     this.agendamentoForm = new FormGroup({
-      data: new FormControl<Date | null>(null, [Validators.required, Validators.nullValidator]),
+      data: new FormControl<string | null>(this.today, [Validators.required, Validators.nullValidator]),
       horario: new FormControl<string>("", [Validators.required, Validators.minLength(5)]),
-      cliente: new FormControl<string>("", [Validators.required, Validators.minLength(3), Validators.maxLength(50)])
-    })  
+      cliente: new FormControl<string>("", [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      celular: new FormControl<string>("", [Validators.required, Validators.minLength(11), Validators.maxLength(11)])
+    })
+
+    this.fetchAgendamentosPorDia()
+  }
+
+  get data(){
+    return this.agendamentoForm?.get("data")!;
+  }
+
+  get cliente(){
+    return this.agendamentoForm?.get("cliente")!;
+  }
+
+  get horario(){
+    return this.agendamentoForm?.get("horario")!;
+  }
+
+  get celular(){
+    return this.agendamentoForm?.get("celular")!;
   }
 
   fetchAgendamentosPorDia(){
-    console.log(this.agendamentoForm?.get("data")?.value)
+    console.log("Data" + this.data.value)
     //falta passar o valor do input date para fazer a requisição
-    
   }
 
   onSubmit(){
