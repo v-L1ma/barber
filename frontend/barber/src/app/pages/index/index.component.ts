8c28c28f90c012f047d59e70be766f233c5b0a08
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-06-11 18:26:33 -0300

    criando menu mobile e melhorando responsividade

--- frontend/barber/src/app/pages/index/index.component.ts
+++ frontend/barber/src/app/pages/index/index.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-index',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
+
+  isOpen:boolean = false;
+
+  openMenu(){
+    this.isOpen = !this.isOpen;
+  }
 
 }
