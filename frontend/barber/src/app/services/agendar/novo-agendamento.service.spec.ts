58524841f690290a36a5fa34f29bd80aed8d5f81
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-06-24 22:57:16 -0300

    test: adicionando testes unitarios nos services

--- frontend/barber/src/app/services/agendar/novo-agendamento.service.spec.ts
+++ frontend/barber/src/app/services/agendar/novo-agendamento.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NovoAgendamentoService } from './novo-agendamento.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('NovoAgendamentoService', () => {
   let service: NovoAgendamentoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(NovoAgendamentoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create a new agendamento and return a message',()=>{
+
+    const expectedResponse = {
+      message: "Agendamento feito com sucesso"
+    }
+
+    const agendamento = {
+        data: "2025-02-13",
+        horario: "12:00",
+        cliente: "Vinicius",
+        servico: "Corte"
+    }
+
+    service.agendar(agendamento).subscribe();
+
+    const req = httpMock.expectOne("https://barberbookingapi.onrender.com/agendamento");
+    expect(req.request.method).toBe('POST');
+
+    req.flush(expectedResponse);
+
+  });
+
 });
--- frontend/barber/src/app/services/deletarAgendamento/deletar-agendamento.service.spec.ts
+++ frontend/barber/src/app/services/deletarAgendamento/deletar-agendamento.service.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DeletarAgendamentoService } from './deletar-agendamento.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('DeletarAgendamentoService', () => {
   let service: DeletarAgendamentoService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(DeletarAgendamentoService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(()=>{
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it("should delete an agendamento and return a message", ()=>{
+
+    const expectedResponse = {
+      message: "Agendamento excluido com sucesso!"
+    }
+
+    service.delete(1).subscribe(response=> expect(response).toBe(expectedResponse));
+
+    const req = httpMock.expectOne("https://barberbookingapi.onrender.com/agendamento/1");
+    expect(req.request.method).toBe('DELETE')
+
+    req.flush(expectedResponse);
+
   });
 });
