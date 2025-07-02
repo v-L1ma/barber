f3db1dc75b3a356815509321db6bdbd19cb6cc80
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-07-01 22:35:08 -0300

    feat: adicionando testes unitarios para criar agendamento

---
+++ backend/src/test/java/com/barber/barber/application/usecases/criarAgendamento/criarAgendamentoUseCaseTest.java
@@ -0,0 +1,102 @@
+package com.barber.barber.application.usecases.criarAgendamento;
+
+import com.barber.barber.application.services.IAgendamentoService;
+import com.barber.barber.domain.entities.Agendamento.Agendamento;
+import com.barber.barber.domain.exceptions.AgendamentoJaExisteException;
+import com.barber.barber.domain.exceptions.AgendamentoNaoPodeSerNoPassadoException;
+import com.barber.barber.domain.exceptions.CamposObrigatoriosException;
+import com.barber.barber.infra.web.DTOs.CadastrarAgendamentoDto;
+import com.barber.barber.infra.web.DTOs.CadastrarAgendamentoResponseDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class criarAgendamentoUseCaseTest {
+
+    @Mock
+    private IAgendamentoService agendamentoService;
+
+    @InjectMocks
+    private criarAgendamentoUseCase criarAgendamentoUseCase;
+
+    @Test
+    @DisplayName("Deve lançar uma exception de Campos obrigatórios")
+    void deveLancarExceptionCamposObrigatorios() {
+
+        CadastrarAgendamentoDto dto = new CadastrarAgendamentoDto("Vinicius", null, null, "Barba");
+
+        assertThrows(CamposObrigatoriosException.class, ()->{
+            criarAgendamentoUseCase.executar(dto);
+        });
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exception se a data do agendamento for no passado")
+    void deveLancarExceptionAgendamentoNaoPodeSerNoPassado(){
+
+        CadastrarAgendamentoDto dto = new CadastrarAgendamentoDto(
+                "Vinicius",
+                LocalDate.now().minusMonths(2),
+                LocalTime.of(13,0), "Barba"
+        );
+
+        assertThrows(AgendamentoNaoPodeSerNoPassadoException.class, ()->{
+            criarAgendamentoUseCase.executar(dto);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exception se já existir um agendamento na mesma data e hora")
+    void deveLancarExceptionAgendamentoJaExiste(){
+        CadastrarAgendamentoDto dto = new CadastrarAgendamentoDto(
+                "Vinicius",
+                LocalDate.now().plusDays(2),
+                LocalTime.of(13,0), "Barba"
+        );
+
+        Agendamento agendamento = new Agendamento(
+                1,
+                "Vinicius",
+                LocalDate.now().plusDays(2),
+                LocalTime.of(13,0), "Barba"
+        );
+
+        List<Agendamento> agendamentos = List.of(agendamento);
+
+        Mockito.when(agendamentoService.listarAgendamentos()).thenReturn(agendamentos);
+
+        assertThrows(AgendamentoJaExisteException.class,()->{
+            criarAgendamentoUseCase.executar(dto);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma response de sucesso")
+    void deveRetornarUmaResponseDeSucesso(){
+
+        CadastrarAgendamentoDto dto = new CadastrarAgendamentoDto(
+                "Vinicius",
+                LocalDate.now().plusDays(2),
+                LocalTime.of(13,0), "Barba"
+        );
+
+        Mockito.when(agendamentoService.listarAgendamentos()).thenReturn(Collections.emptyList());
+
+        CadastrarAgendamentoResponseDto response = criarAgendamentoUseCase.executar(dto);
+
+        assertEquals("Agendamento feito com sucesso", response.message());
+    }
+}
