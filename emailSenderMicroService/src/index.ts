3d5dbbae419a6deac580e0908e9dc5206bdd8d0b
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-07-23 22:14:31 -0300

    feat: expondo uma porta

--- emailSenderMicroService/src/index.ts
+++ emailSenderMicroService/src/index.ts
@@ -1,6 +1,15 @@
 import RabbitMQConnection from "./services/rabbitMQConnection";
+import http from 'http';
+
+const port = process.env.PORT || 3000;
+
+http.createServer((_, res) => {
+  res.writeHead(200);
+  res.end('Servidor ativo.');
+}).listen(port, () => {
+  console.log(`Servidor escutando na porta ${port}`);
+});
 
 const rabbitMQConnection = new RabbitMQConnection();
 
 rabbitMQConnection.initialize();
-
