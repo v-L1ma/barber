0de12b776818c38b65eb72f12757a5582f0a1eca
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-07-25 19:56:39 -0300

    feat: adicionando loading enquanto valida o token

--- frontend/barber/src/app/pages/index/index.component.ts
+++ frontend/barber/src/app/pages/index/index.component.ts
@@ -1,19 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoadingComponent } from "../../components/loading/loading/loading.component";
 
 @Component({
   selector: 'app-index',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, LoadingComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
 
   isOpen:boolean = false;
+  button:string = "";
+  isLoading:boolean = false;
 
   openMenu(){
     this.isOpen = !this.isOpen;
+  }
+
+  setIsloading(button:string){
+    this.isLoading = !this.isLoading;
+    this.button = button;
   }
 
 }
